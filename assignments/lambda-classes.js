f0112c6cf483348c490d92cebef5e3a74ce445ef
Fresco Fresco <fresco@Frescos-MacBook-Pro.local>
2019-08-23 01:59:57 -0400

    added all student, instructor, project manager classes, created at least 2 objects for each. Tested code to make sure everything is hooked up properly. MVP done

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,20 +1,4 @@
-// CODE here for your Lambda Classes
-​
-/*
-- We have a school to build here! This project will get you used to thinking about classes in JavaScript and building them from a brand new data set.
-* Lambda personnel can be broken down into three different types of people.
-    - Instructors - extensions of Person
-    - Students - extensions of Person
-    - Project Managers - extensions of Instructors
-IMPORTANT - You'll need to create 2 - 3 objects for each class and test them according to their unique Attributes. For example:
-*/
-​
-// * First we need a Person class. This will be our `base-class`
-// * Person receives `name` `age` `location` all as props
-// * Person receives `speak` as a method.
-// * This method logs out a phrase `Hello my name is Fred, I am from Bedrock` where `name` and `location` are the object's own props
-
-​class Person {
+class Person {
     constructor(person_attrs){
         this.name = person_attrs.name;
         this.age = person_attrs.age;
@@ -22,35 +6,16 @@ IMPORTANT - You'll need to create 2 - 3 objects for each class and test them acc
     }
     speak() { return `Hello, my name is ${this.name}, I am from ${this.location}. `}
 }
-​
-// * Instructor has the following unique props:
-//   * `specialty` what the Instructor is good at i.e. 'redux'
-//   * `favLanguage` i.e. 'JavaScript, Python, Elm etc.'
-//   * `catchPhrase` i.e. `Don't forget the homies`
-// * Instructor has the following methods:
-//   * `demo` receives a `subject` string as an argument and logs out the phrase 'Today we are learning about {subject}' where subject is the param passed in.
-//   * `grade` receives a `student` object and a `subject` string as arguments and logs out '{student.name} receives a perfect score on {subject}'
-​
 class Instructor extends Person {
-    constructor(​i_attrs){
+    constructor(i_attrs){
         super(i_attrs);
         this.specialty = i_attrs.specialty;
         this.favLanguage = i_attrs.favLanguage;
         this.catchPhrase = i_attrs.catchPhrase;
     }
-    demo(subject){ return `Today we are learning about {subject}.`}
-    grade(){ return `${student.name} receives a perfect score on {subject}`}
+    demo(subject){ return `Today we are learning about ${subject}.`}
+    grade(student, subject){ return `${student.name} receives a perfect score on ${subject}`}
 }
-
-​
-// * ProjectManagers are extensions of Instructors
-// * Project Manger has the following unique props:
-// * `gradClassName`: i.e. CS1
-//   * `favInstructor`: i.e. Sean
-// * ProjectManagers have the following Methods:
-//   * `standUp` a method that takes in a slack channel and logs `{name} announces to {channel}, @channel standy times!​​​​​
-//   * `debugsCode` a method that takes in a student object and a subject and logs out `{name} debugs {student.name}'s code on {subject}`
-​
 class ProMgr extends Instructor {
     constructor(promgr_attrs){
         super(promgr_attrs);
@@ -60,17 +25,6 @@ class ProMgr extends Instructor {
     standUp(channel){ return `${this.name} announces to ${channel}, @channel standy times!`}
     debugsCode(student, subject){ return `${this.name} debugs ${student.name}'s code on ${subject}` }
 }
-​
-
-// * Student has the following unique props:
-//   * `previousBackground` i.e. what the Student used to do before Lambda School
-//   * `className` i.e. CS132
-//   * `favSubjects`. i.e. an array of the student's favorite subjects ['Html', 'CSS', 'JavaScript']
-// * Student has the following methods:
-//   * `listsSubjects` a method that logs out all of the student's favoriteSubjects one by one.
-//   * `PRAssignment` a method that receives a subject as an argument and logs out that the `student.name has submitted a PR for {subject}`
-//   * `sprintChallenge` similar to PRAssignment but logs out `student.name has begun sprint challenge on {subject}`
-​
 class Student extends Person {
     constructor(student_attrs){
         super(student_attrs);
@@ -78,17 +32,100 @@ class Student extends Person {
         this.className = student_attrs.className;
         this.favSubjects = student_attrs.favSubjects;
     }
-    listsSubjects(){ return `${this.name}'s favorite subjects are {this.favSubjects}.` }
+    listSubjects(subject){ return `${this.name}'s favorite subjects are ${this.favSubjects}.` }
     PRAssignment(subject){ return `${this.name} has submitted a PR for ${subject}` }
-    sprintChallenge(){ }
+    sprintChallenge(subject){ return `${this.name} has begun sprint challenge on ${subject}`}
+    eats(){ return `${this.name} eats an entire bag of oreos on lecture breaks.`}
 }
 
+// CREATE 
+const studentBella = new Student({
+    name: 'Bella',
+    age: '1',
+    location: 'San Antonio, Texas',
+    previousBackground: 'Dog',
+    className: 'Dog Park Etiquette',
+    favSubjects: 'cuddling, belly rubs, snack time'
+});
 
-/*
-Stretch Problem
-* Extend the functionality of the Student by adding a prop called grade and setting it equal to a number between 1-100.
-* Now that our students have a grade build out a method on the Instructor (this will be used by BOTH instructors and PM's) that will randomly add or subtract points to a student's grade. Math.random will help.
-* Add a graduate method to a student.
-    - This method, when called, will check the grade of the student and see if they're ready to graduate from Lambda School
-    - If the student's grade is above a 70% let them graduate! Otherwise go back to grading their assignments to increase their score.
-*/
+const studentIdiot = new Student({
+    name: 'Dakotah',
+    age: '25',
+    location: 'dorklandia, USA',
+    previousBackground: 'still idiot',
+    className: 'js',
+    favSubjects: 'eating, sleeping, eating'
+});
+
+const studentOreoEater = new Student({
+    name: 'Kevin',
+    age: '99',
+    location: 'bahston',
+    previousBackground: 'nerd',
+    className: 'pirate',
+    favSubjects: 'specialty oreos'
+});
+
+
+const instructorPhysics = new Instructor({
+    name: 'Dan',
+    age: '43',
+    location: 'NYC',
+    previousBackground: 'Mechanic',
+    className: 'Physics',
+    favSubjects: 'philosophy, astrophysics, pizza',
+    specialty: 'teaching',
+    favLanguage: 'french',
+    catchPhrase: 'eureka',
+});
+
+const instructorGenderPolitics = new Instructor({
+    name: 'Sam Hyde',
+    age: '34',
+    location: 'Massachusetts',
+    previousBackground: 'galaxy brain genius',
+    className: '2070 Paradigm Shift',
+    favSubjects: 'bowl cuts',
+    specialty: 'stand up comedy',
+    favLanguage: 'english',
+    catchPhrase: 'we\'re doing a quad stack',
+});
+
+const proMgrOne = new ProMgr({
+    name: 'Brit',
+    age: '29',
+    location: 'Canada',
+    previousBackground: 'teachers',
+    className: 'Full Stack Web Dev',
+    favSubjects: 'Ada',
+    specialty: 'clapping back in Slack',
+    favLanguage: 'Canadian',
+    catchPhrase: 'I\'m going to put you guys in breakout rooms',
+    gradClassName: 'web23',
+    favInstructor: 'Ada',
+});
+
+const proMgrTwo = new ProMgr({
+    name: 'Christian Ford',
+    age: '22',
+    location: 'Lumber Yard',
+    previousBackground: 'tree chopper',
+    className: 'tree chopping',
+    favSubjects: 'trees',
+    specialty: 'making sure we actually do our work',
+    favLanguage: 'javascript',
+    catchPhrase: 'do the career form',
+    gradClassName: 'web23',
+    favInstructor: 'Brit',
+});
+
+// TEST
+
+
+console.log(studentBella.listSubjects());
+console.log(studentOreoEater.eats());
+console.log(studentIdiot.sprintChallenge('memeology 101'));
+console.log(instructorGenderPolitics.grade(studentBella, 'gender politics'));
+console.log(instructorPhysics.demo('cinematography'));
+console.log(proMgrOne.standUp('#web23'));
+console.log(proMgrTwo.debugsCode(studentIdiot, 'javascript'));
